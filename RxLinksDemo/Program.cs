891a7764c1c9abdd7c065dc4f2654dff0e9a5b42
Hakim Ghods <ghods.hakim@hotmail.com>
2020-02-27 03:12:34 +0330

    Add project files.

---
+++ RxLinksDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RxLinksDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var links = await System.IO.File.ReadAllLinesAsync(@"E:\Projects\Data\urls.txt");
+
+            var query = links.ToObservable();
+
+            var sub=query.Subscribe(async (url) => {
+
+                var client = new HttpClient();
+                try
+                {
+                    Console.WriteLine($" {url} started --- ");
+                    client.Timeout = TimeSpan.FromSeconds(100);
+
+                    var page = await client.GetAsync(url, HttpCompletionOption.ResponseContentRead);
+                    var pageSize = page.Content.Headers.ContentLength.Value;
+                    Console.WriteLine($" --- {url}                    (Size : {pageSize}) [{Thread.CurrentThread.ManagedThreadId}]");
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine($" {url}      (Size : {0}) [{Thread.CurrentThread.ManagedThreadId}] - ERR");
+                }
+
+            });
+
+            
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
